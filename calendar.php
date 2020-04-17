c30f5ea38738667c2ffd5fe0bd3384b88e9987a0
張 建文 <chang180@gmail.com>
2020-04-17 19:16:30 +0800

    Seperate HTML & CSS

--- calendar.php
+++ calendar.php
@@ -5,95 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>萬年曆</title>
-    <style>
-        body {
-            margin: 0;
-            font-family: "Microsoft JhengHei", Arial, Helvetica, sans-serif;
-        }
-
-        .container {
-            width: 100vh;
-            border: 0;
-            text-align: center;
-            background: rgb(240, 229, 135);
-            margin: auto;
-
-        }
-
-        .calendar {
-            width: 60vh;
-            height: 600px;
-            text-align: center;
-            background: rgb(170, 216, 189);
-
-        }
-
-        .calendar .week td {
-            background: rgb(130, 222, 238);
-            font-size: 30px;
-        }
-
-        .month {
-            width: 60vh;
-            background: aqua;
-            font-size: 20px;
-        }
-
-        .calendar td {
-            font-size: 25px;
-            background: #becdd3;
-        }
-
-        .calendar td:hover {
-            background: #ff0000;
-            font-weight: bold;
-        }
-
-        .date {
-            font-size: 50px;
-            font-weight: bold;
-        }
-
-        .rCol {
-            background: url("https://picsum.photos/400/640/?random=1");
-        }
-
-        .calendar .selected {
-            background: #3e70db;
-        }
-
-        .month .selected {
-            background: blueviolet
-        }
-
-        .rCol {
-            width: 40vh;
-        }
-    </style>
-
+    <link rel="stylesheet" href="style.css">
 
 </head>
 
 
 <body>
-<div>
-    <form action="?" method='get'>
-    年份:<input type="number" name="year">
-    <input type="submit" value="產生年曆">
+    <div>
+        <form action="?" method='get'>
+            年份:<input type="number" name="year">
+            月份:<input type="number" name="month">
+            <input type="submit" value="查詢">
 
-    </form>
-</div>
-<?php
+        </form>
+    </div>
 
-// 利用表單輸入獲得年份值，若沒有則使用今年做為值
-if(isset($_GET["year"])){
-    $year=$_GET['year'];
-}else{
-    $year=date("Y");
-}
+    <?php
 
-echo "<h4 style='text-align:center'>西元",$year,"年曆</h4>";
-?>
+    // 利用表單輸入獲得年份值，若沒有則使用今年做為值
+    if (isset($_GET["year"])) {
+        $year = $_GET['year'];
+    } else {
+        $year = date("Y");
+    }
+    // 利用表單輸入獲得月份值，若沒有則使用本月做為值
+    if (isset($_GET["month"])) {
+        $month = $_GET['month'];
+    } else {
+        $month = date("m");
+    }
+
+    echo "<h4 style='text-align:center'>西元", $year, "年", $month, "月</h4>";
+    ?>
 
     <table class="container">
         <tr>
@@ -135,8 +78,8 @@ echo "<h4 style='text-align:center'>西元",$year,"年曆</h4>";
                     <?php
                     // 開工用php填日期了
                     $today = date("d-m-Y");
-                    // 先定出月份的第1天
-                    $firstDay= date("1-m-Y");
+                    // 先定出本月份的第1天
+                    $firstDay = date("1-m-Y");
                     // 第一天是該週的第幾天
                     // $firstDayWeek = getdate(strtotime(date("1-m-Y")));
                     $firstDayWeek = date("w", strtotime($firstDay));
@@ -144,16 +87,16 @@ echo "<h4 style='text-align:center'>西元",$year,"年曆</h4>";
                     // $weekday = $firstDay["wday"];
                     // $days=date("t",strtotime(date("Y-m")."-1"));
                     // 當月份總共有幾天
-                    $days = date("t",strtotime($firstDay));
+                    $days = date("t", strtotime($firstDay));
                     // 當月份跨了幾週，這很重要
-                    $totalWeeks = ceil(($days+$firstDayWeek)/7);
+                    $totalWeeks = ceil(($days + $firstDayWeek) / 7);
 
 
                     for ($i = 0; $i < $totalWeeks; $i++) {
 
                         echo "<tr>";
                         for ($j = 0; $j < 7; $j++) {
-                
+
                             if ($i == 0 && $j < $firstDayWeek) {
                                 echo "<td>";
                                 echo "</td>";
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 這是萬年曆作業的repo
 
-以fork的方式更新git classroom的repo
+以fork之後再做pull request的方式更新git classroom的repo
 
 使用PHP進行建構，再加以CSS樣式美化
 
